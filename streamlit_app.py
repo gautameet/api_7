b9c5e077556b02036374bf9194a010ea9d8e24c3
gautameet <145467217+gautameet@users.noreply.github.com>
2024-01-04 01:34:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from PIL import Image
 import requests
 #import plotly.graph_objects as go
-import plotly.express as px
+#import plotly.express as px
 import plotly.figure_factory as ff
 import json
 import pandas as pd
