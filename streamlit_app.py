dc44d7bee7a8e138df774812f439812c52475d8f
gautameet <145467217+gautameet@users.noreply.github.com>
2024-01-04 02:16:07 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ import requests
 
 st.title("Credit Score Dashboard")
 
-#st.write("Crédit Score Dashboard")
+#st.write("💳💵 Crédit Score Dashboard")
 ##################################################
 # Page configuration inistiatlisation
 #st.set_page_config(page_title="Credit Score Dashboard", page_icon="💳💵", layout="wide", initial_sidebar_state="expanded", menu_items=None)
