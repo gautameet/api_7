c4c6ffaa0782c83d8556fc9df1c34c8c12deb16a
gautameet <145467217+gautameet@users.noreply.github.com>
2024-01-04 03:50:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,14 @@
 import streamlit as st
 from PIL import Image
 import requests
-#import plotly.graph_objects as go
-#import plotly.express as px
-#import plotly.figure_factory as ff
-#import json
-#import pandas as pd
-#import numpy as np
-#import matplotlib.pyplot as plt
-#import seaborn as sns
+import plotly.graph_objects as go
+import plotly.express as px
+import plotly.figure_factory as ff
+import json
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 st.title("💳💵 Credit Score Dashboard")
 
