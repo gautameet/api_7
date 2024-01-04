c38ba509ea93d8d72d384a48585174390f0fc818
gautameet <145467217+gautameet@users.noreply.github.com>
2024-01-04 01:30:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded")
 
-    alt.themes.enables("dark")
+alt.themes.enables("dark")
 
 
 ##################################################
