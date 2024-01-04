9d8d1d28ebc1250306414ecae34ec22232eccaa5
gautameet <145467217+gautameet@users.noreply.github.com>
2024-01-04 02:22:24 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,18 @@ st.title("💳💵 Credit Score Dashboard")
 #st.write("💳💵 Crédit Score Dashboard")
 ##################################################
 
-
 # Page configuration inistiatlisation
+st.set_page_config(
+    page_title="Ex-stream-ly Cool App",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
 #st.set_page_config(page_title="Credit Score Dashboard", page_icon="💳💵", layout="wide", initial_sidebar_state="expanded", menu_items=None)
 #alt.themes.enable("dark")
 
