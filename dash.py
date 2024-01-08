08b6c4a33d738e8e5e6b3e080cf23a9274250f09
gautameet <145467217+gautameet@users.noreply.github.com>
2024-01-08 12:55:35 +0100

    Update dash.py

--- dash.py
+++ dash.py
@@ -60,7 +60,7 @@ if page == "Home":
                 "- **Global Interpretation**: Information regarding the comparisons and similarity between the customer according to the database.")
 
 if page == "Client Information":
-    st.title(""💳💵 Credit Score Dashboard - Client Information - Page")
+    st.title(""Credit Score Dashboard - Client Information - Page")
 
     st.write("To analyse your demand, please click the button below :")
     button_start = st.button("Demand Status")
