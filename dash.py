c39f438c02f7547c05437cff44277cedd91419ef
gautameet <145467217+gautameet@users.noreply.github.com>
2024-01-13 17:48:16 +0100

    Update dash.py

--- dash.py
+++ dash.py
@@ -21,14 +21,14 @@ model = pickel.load(open("./Results/model.pkl","wb")
 
                     
 # Page configuration initialisation
-st.set_page_config(
+  st.set_page_config(
   page_title="Credit Score Dashboard", 
   page_icon="💳💵",
   layout="wide",
   initial_sidebar_state="expanded",
   menu_items=None)
 
-alt.themes.enable("dark")
+  alt.themes.enable("dark")
 
 # Sidebar
 with st.sidebar:
