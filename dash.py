6f7b0b3f20888d846184d303e91ca6463c6d49a0
gautameet <145467217+gautameet@users.noreply.github.com>
2024-01-14 03:09:54 +0100

    Update dash.py

--- dash.py
+++ dash.py
@@ -24,22 +24,21 @@ import pandas as pd
 pkl_model= open("./model.pkl","rb")
 model = pickle.load(pkl_model)
 
-zip_file_path = './data_train.zip'
-
-try:
-    with ZipFile(zip_file_path, 'r') as zip_train:
-        # Your code to handle the zip file
-        # For example, listing the contents of the zip file:
-        print(zip_train.namelist())
-except zipfile.BadZipFile:
-    print(f"Error: {zip_file_path} is not a valid zip file.")
-except FileNotFoundError:
-    print(f"Error: File not found at {zip_file_path}.")
-except Exception as e:
-    print(f"An unexpected error occurred: {e}")
+zip_file_train = './data_train.zip'
+zip_file_test = './data_test.zip'
+#try:
+with ZipFile(zip_file_train, 'r') as zip_train:
+    #print(zip_train.namelist())
+#except zipfile.BadZipFile:
+    #print(f"Error: {zip_file_path} is not a valid zip file.")
+#except FileNotFoundError:
+    #print(f"Error: File not found at {zip_file_path}.")
+#except Exception as e:
+    #print(f"An unexpected error occurred: {e}")
 
 #with ZipFile('./data_train.zip','r') as zip_train:
-    df_train=pd.read_csv(zip_train.open('data_train.csv'))
+df_train=pd.read_csv(zip_train.open('data_train.csv'))
+df_test=pd.read_csv(zip_test.open('data_test.csv'))    
 
     X_train= shap.TreeExplainer(model, df_train)
 
