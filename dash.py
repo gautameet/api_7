5789332933d6ac503f7a1936b0eb69f1770051af
gautameet <145467217+gautameet@users.noreply.github.com>
2024-01-14 05:53:28 +0100

    Update dash.py

--- dash.py
+++ dash.py
@@ -73,116 +73,116 @@ try:
     
     st.write("By Amit GAUTAM")
                    
-if page == "Home":
-    st.title("💳💵 Credit Score Dashboard - Home Page")
-    ".\n"
-    #.\n"
+    if page == "Home":
+        st.title("💳💵 Credit Score Dashboard - Home Page")
+        ".\n"
+        #.\n"
   
-    st.markdown("This is an interactive dashboard website which lets the clients to know about their credit demands\n"
-                "approved ou refused. The predictions are calculted automatically with the help of machine learning algorithm.\n"
+        st.markdown("This is an interactive dashboard website which lets the clients to know about their credit demands\n"
+                    "approved ou refused. The predictions are calculted automatically with the help of machine learning algorithm.\n"
                                 
-                "\nThis dashboard is composed of following pages :\n"
-                "- **Customer**: to find out all the information related to the customer.\n"
-                "- **Local Interpretation**: Information regarding the reasons for accepting or refusing the credits of a particular customer.\n"
-                "- **Global Interpretation**: Information regarding the comparisons and similarity between the customer according to the database.")
+                    "\nThis dashboard is composed of following pages :\n"
+                    "- **Customer**: to find out all the information related to the customer.\n"
+                    "- **Local Interpretation**: Information regarding the reasons for accepting or refusing the credits of a particular customer.\n"
+                    "- **Global Interpretation**: Information regarding the comparisons and similarity between the customer according to the database.")
 
-if page == "Customer":
-    st.title("💳💵 Credit Score Dashboard - Customer")
+    if page == "Customer":
+        st.title("💳💵 Credit Score Dashboard - Customer")
     
-# Display customer information
-    ".\n"
-    st.title("Welcome to the customer page")
-    
-    ".\n"
-    st.write("Please click the below button to enter:")
-    
-    button_start = st.button("Your ID number")
-    if button_start:
-        if id_client_dash != '<Select>':
-            # Calculates the prediction and displays the results"
-            st.markdown("Result of your request")
-            probability, decision = get_prediction(id_client_dash)
-            
-            if decision == 'Approved':
-                st.success("Loan approved")
-            else:
-                st.error("Loan refused")
-
-          
-            # Affichage de la jauge
-            jauge_score(probability)
-          
     # Display customer information
-    #with st.expander("Display customer information", expanded=False):
-        #st.info("The customer information are:")
-        #st.write(pd.DataFrame(data_test.loc[data_test['SK_ID_CURR'] == id_client_dash]))
-
-if page == "Local interpretation":
-    st.title("💳💵 Credit Score Dashboard - Local Interpretation - Page")
-
-    locale = st.checkbox("Local Interpretation")
-    if locale:
-        st.info("Local Interpretation prediction")
-        #shap_val = get_shap_val_local(id_client_dash)
-        nb_features = st.slider("Number of variables to display", 0, 20, 10)
-        
-    # Diplaying waterfall plot : shap local
-    #fig = shap.waterfall_plot(shap_val, max_display=nb_features, show=False)
-    #st.pyplot(fig)
-
-        with st.expander("Graphical presentation", expanded=False):
-            st.caption("Displaying the features that influence the decision locally (for a particular custimer)")
-
-if page == "Global interpretation":
-    st.title("💳💵 Credit Score Dashboard - Global interpretation - Page")
-   
-    # Création du dataframe de voisins similaires
-     #data_voisins = df_voisins(id_client_dash)
-
-    globale = st.checkbox("Global Importance")
-    if globale:
-        st.info("Global Importance")
-        #shap_values = get_shap_val()
-        #data_test_std = minmax_scale(data_test.drop('SK_ID_CURR', axis=1), 'std')
-        nb_features = st.slider("Number of varaibles to display", 0, 20, 10)
-        #fig, ax = plt.subplots()
-       
-        # Displaying summary plot : shap global
-        #ax = shap.summary_plot(shap_values[1], data_test_std, plot_type='bar', max_display=nb_features)
-        #st.pyplot(fig)
+        ".\n"
+        st.title("Welcome to the customer page")
     
-        with st.expander("Graphical presentation", expanded=False):
-            st.caption("Displaying the features that influence the decision in global scenario.")
+        ".\n"
+        st.write("Please click the below button to enter:")
+    
+        button_start = st.button("Your ID number")
+        if button_start:
+            if id_client_dash != '<Select>':
+                # Calculates the prediction and displays the results"
+                st.markdown("Result of your request")
+                probability, decision = get_prediction(id_client_dash)
+            
+                if decision == 'Approved':
+                    st.success("Loan approved")
+                else:
+                    st.error("Loan refused")
 
-    distrib = st.checkbox("Distribution comparision")
-    if distrib:
-        st.info("Disptribution comparaison wtih other variables from the data")
           
-        #Possibilité de choisir de comparer le client sur l'ensemble de données ou sur un groupe de clients similaires
-        distrib_compa = st.radio("Choose the type of comparision :", ('All', 'Similar clients'), key='distrib')
-    
-        #list_features = list(data_train.columns)
-        #list_features.remove('SK_ID_CURR')
-           
-        #Affichage des distributions des variables renseignées
-        with st.spinner(text="Charging graphs..."):
-            col1, col2 = st.columns(2)
-            with col1:
-                feature1 = st.selectbox("Please choose one feature", list_features, index=list_features.index('AMT_CREDIT'))
-                if distrib_compa == 'All':
-                    distribution(feature1, id_client_dash, df_train)
-                else:
-                    distribution(feature1, id_client_dash, data_voisins)
-             with col2:
-                feature2 = st.selectbox("Please choose one feature", list_features, index=list_features.index('EXT_SOURCE_2'))
-                if distrib_compa == 'All':
-                    distribution(feature2, id_client_dash, df_train)
-                else:
-                    distribution(feature2, id_client_dash, data_voisins)
+                # Affichage de la jauge
+                jauge_score(probability)
+          
+        # Display customer information
+        #with st.expander("Display customer information", expanded=False):
+            #st.info("The customer information are:")
+            #st.write(pd.DataFrame(data_test.loc[data_test['SK_ID_CURR'] == id_client_dash]))
 
-            with st.expander("Distribution explanation", expanded=False):
-                st.caption("You can select the feature that you like to observe the distribution. "
-                            "Blue shows the clients distribution not having credit default and their "
-                            "loan is considered to be approved (Loan). Orange shows the clients distribution "
-                            "having credit default and their loan is considered to be refused. "
-                            "The green dotted line indicates where the customer stands in relation to other customers.")
+    if page == "Local interpretation":
+        st.title("💳💵 Credit Score Dashboard - Local Interpretation - Page")
+
+        locale = st.checkbox("Local Interpretation")
+        if locale:
+            st.info("Local Interpretation prediction")
+            #shap_val = get_shap_val_local(id_client_dash)
+            nb_features = st.slider("Number of variables to display", 0, 20, 10)
+        
+        # Diplaying waterfall plot : shap local
+        #fig = shap.waterfall_plot(shap_val, max_display=nb_features, show=False)
+        #st.pyplot(fig)
+
+            with st.expander("Graphical presentation", expanded=False):
+                st.caption("Displaying the features that influence the decision locally (for a particular custimer)")
+
+    if page == "Global interpretation":
+        st.title("💳💵 Credit Score Dashboard - Global interpretation - Page")
+   
+        # Création du dataframe de voisins similaires
+         #data_voisins = df_voisins(id_client_dash)
+
+        globale = st.checkbox("Global Importance")
+        if globale:
+            st.info("Global Importance")
+            #shap_values = get_shap_val()
+            #data_test_std = minmax_scale(data_test.drop('SK_ID_CURR', axis=1), 'std')
+            nb_features = st.slider("Number of varaibles to display", 0, 20, 10)
+            #fig, ax = plt.subplots()
+       
+            # Displaying summary plot : shap global
+            #ax = shap.summary_plot(shap_values[1], data_test_std, plot_type='bar', max_display=nb_features)
+            #st.pyplot(fig)
+    
+            with st.expander("Graphical presentation", expanded=False):
+                st.caption("Displaying the features that influence the decision in global scenario.")
+
+        distrib = st.checkbox("Distribution comparision")
+        if distrib:
+            st.info("Disptribution comparaison wtih other variables from the data")
+          
+            #Possibilité de choisir de comparer le client sur l'ensemble de données ou sur un groupe de clients similaires
+            distrib_compa = st.radio("Choose the type of comparision :", ('All', 'Similar clients'), key='distrib')
+    
+            #list_features = list(data_train.columns)
+            #list_features.remove('SK_ID_CURR')
+           
+            #Affichage des distributions des variables renseignées
+            with st.spinner(text="Charging graphs..."):
+                col1, col2 = st.columns(2)
+                with col1:
+                    feature1 = st.selectbox("Please choose one feature", list_features, index=list_features.index('AMT_CREDIT'))
+                    if distrib_compa == 'All':
+                        distribution(feature1, id_client_dash, df_train)
+                    else:
+                        distribution(feature1, id_client_dash, data_voisins)
+                 with col2:
+                    feature2 = st.selectbox("Please choose one feature", list_features, index=list_features.index('EXT_SOURCE_2'))
+                    if distrib_compa == 'All':
+                        distribution(feature2, id_client_dash, df_train)
+                    else:
+                        distribution(feature2, id_client_dash, data_voisins)
+
+                with st.expander("Distribution explanation", expanded=False):
+                    st.caption("You can select the feature that you like to observe the distribution. "
+                                "Blue shows the clients distribution not having credit default and their "
+                                "loan is considered to be approved (Loan). Orange shows the clients distribution "
+                                "having credit default and their loan is considered to be refused. "
+                                "The green dotted line indicates where the customer stands in relation to other customers.")
