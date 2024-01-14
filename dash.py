f5821275dc781233e9bcb7ba933a3ce35b3ad872
gautameet <145467217+gautameet@users.noreply.github.com>
2024-01-14 08:42:27 +0100

    Update dash.py

--- dash.py
+++ dash.py
@@ -56,11 +56,10 @@ try:
 
 ## Page configuration initialisation
 st.set_page_config(
-        page_title="Credit Score Dashboard",
-        page_icon="💳💵",
-        layout="wide",
-        #initial_sidebar_state="expanded"
-)
+    page_title="Credit Score Dashboard",
+    page_icon="💳💵",
+    layout="wide",
+    initial_sidebar_state="expanded")
 
 # Sidebar
     with st.sidebar:
