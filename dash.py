70d5372bd36103beee55552ea44cb31b5926b4e3
gautameet <145467217+gautameet@users.noreply.github.com>
2024-01-14 04:53:04 +0100

    Update dash.py

--- dash.py
+++ dash.py
@@ -180,5 +180,4 @@ try:
                         "You can select the feature that you like to observe the distribution,"
                         "Blue shows the clients distribution not having credit default and their loan is considered to be approved (Loan),\n"
                         "\nOrange shows the clients distribution having credit default and their loan is considered to be refused,\n"
-                        "\nThe green dotted line indicates where the customer stands in relation to other customers."
-                    )
+                        "\nThe green dotted line indicates where the customer stands in relation to other customers.")
