0d179c9572ef519e9af7d9096df5c85770b7db8d
gautameet <145467217+gautameet@users.noreply.github.com>
2024-01-14 09:22:59 +0100

    Update dash.py

--- dash.py
+++ dash.py
@@ -78,10 +78,10 @@ with st.sidebar:
     customer_id_list = list(df_test['SK_ID_CURR'])
     customer_id_list.insert(0, '<Select>')
     customer_id_dash = st.selectbox("Customer_id", customer_id_list)
-    if customer_id_dash != '<Select>':
-         st.write(f'You have chosen the Customer ID: {customer_id_dash}')
-    else:
-         st.info("Please select a customer ID.")
+    #if customer_id_dash != '<Select>':
+         #st.write(f'You have chosen the Customer ID: {customer_id_dash}')
+    #else:
+         #st.info("Please select a customer ID.")
                             
          st.markdown("""---""")
     
