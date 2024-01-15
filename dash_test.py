49501c9da80ea4c2ae60893d0b2c7b259bb8505d
gautameet <145467217+gautameet@users.noreply.github.com>
2024-01-15 12:35:01 +0100

    Update dash_test.py

--- dash_test.py
+++ dash_test.py
@@ -20,3 +20,12 @@ st.set_page_config(
     page_icon="💳💵",
     layout="wide",
     initial_sidebar_state="expanded")
+
+# Sidebar
+with st.sidebar:
+    logo_path = "logo.png"
+    try:
+        logo = Image.open(logo_path)
+        st.image(logo, width=180)
+    except FileNotFoundError:
+        st.error(f"Error: Logo file not found at {logo_path}")
