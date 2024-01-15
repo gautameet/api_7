7ba421b170962a9ca939d92115f884e88a27fcd4
gautameet <145467217+gautameet@users.noreply.github.com>
2024-01-15 14:07:34 +0100

    Update dash_test.py

--- dash_test.py
+++ dash_test.py
@@ -56,6 +56,9 @@ if page == "Home":
 
 if page == "Customer":
     st.title("💳💵 Credit Score Dashboard - Customer Page")
+with st.sidebar:
+    st.selectbox["Enter your ID"]
+    
     
 #Display customer information
     st.title("Welcome et Bienvenue")
