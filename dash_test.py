8e23f109bb07cba43d477464ecfbc97a3872db3c
gautameet <145467217+gautameet@users.noreply.github.com>
2024-01-15 16:30:37 +0100

    Update dash_test.py

--- dash_test.py
+++ dash_test.py
@@ -26,7 +26,7 @@ with st.sidebar:
     logo_path = "logo.png"
     try:
         logo = Image.open(logo_path)
-        st.image(logo, width=220)
+        st.image(logo, width=250)
     except FileNotFoundError:
         st.error(f"Error: Logo file not found at {logo_path}")
 
