671f6301cc8702b1e8417cb9ea44928b73ee8f9f
gautameet <145467217+gautameet@users.noreply.github.com>
2024-01-24 00:55:25 +0100

    Update dash_test.py

--- dash_test.py
+++ dash_test.py
@@ -18,10 +18,14 @@ feat = ['SK_ID_CURR','TARGET','DAYS_BIRTH','NAME_FAMILY_STATUS','CNT_CHILDREN',
         'DAYS_EMPLOYED','NAME_INCOME_TYPE','AMT_INCOME_TOTAL','AMT_CREDIT','AMT_ANNUITY']
 
 # Original Data
-zip_file = ZipFile('/application_train.zip')
-raw_train = pd.read_csv(zip_file.open('application_train.csv'),usecols=feat)
-zip_file = ZipFile('/application_test.zip')
-raw_test = pd.read_csv(zip_file.open('application_test.csv'),usecols=[f for f in feat if f!='TARGET'])
+zip_file = '/application_train.zip'
+with ZipFile(zip_file, 'r') as zip_app_train:
+        raw_train = pd.read_csv(zip_file.open('application_train.csv'),usecols=feat)
+
+zip_file = '/application_test.zip'
+with ZipFile(zip_file, 'r') as zip_app_test:
+        raw_test = pd.read_csv(zip_file.open('application_test.csv'),usecols=[f for f in feat if f!='TARGET'])
+
 raw_app = raw_train.append(raw_test).reset_index(drop=True)
 del raw_train
 del raw_test
