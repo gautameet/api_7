e8194970a9187715f8efeb9252239fa2633cc980
gautameet <145467217+gautameet@users.noreply.github.com>
2024-01-24 01:04:26 +0100

    Update dash_test.py

--- dash_test.py
+++ dash_test.py
@@ -20,13 +20,13 @@ feat = ['SK_ID_CURR','TARGET','DAYS_BIRTH','NAME_FAMILY_STATUS','CNT_CHILDREN',
 # Original Data
 zip_file = './application_train.zip'
 with ZipFile(zip_file, 'r') as zip_app_train:
-        raw_train = pd.read_csv(zip_app_train.open('application_train.csv'),usecols=feat)
+    raw_train = pd.read_csv(zip_app_train.open('application_train.csv'),usecols=feat)
 
 zip_file = './application_test.zip'
 with ZipFile(zip_file, 'r') as zip_app_test:
-        raw_test = pd.read_csv(zip_app_test.open('application_test.csv'),usecols=[f for f in feat if f!='TARGET'])
+    raw_test = pd.read_csv(zip_app_test.open('application_test.csv'),usecols=[f for f in feat if f!='TARGET'])
 
-        raw_app = raw_train.append(raw_test).reset_index(drop=True)
+raw_app = raw_train.append(raw_test).reset_index(drop=True)
 del raw_train
 del raw_test
 raw_app['AGE'] = raw_app['DAYS_BIRTH'] // (-365)
