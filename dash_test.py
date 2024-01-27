9056215baeb5d7b465cd449dff40093324d409cd
gautameet <145467217+gautameet@users.noreply.github.com>
2024-01-27 07:01:35 +0100

    Update dash_test.py

--- dash_test.py
+++ dash_test.py
@@ -54,9 +54,9 @@ except Exception as e:
 #del raw_train
 #del raw_test
  
-raw_app['YEARS_EMPLOYED'] = raw_app['DAYS_EMPLOYED'] // (-365)
-raw_app['AGE'] = raw_app['DAYS_BIRTH'] // (-365)
-raw_app['CREDIT'] = raw_app['AMT_CREDIT']   
+    raw_app['YEARS_EMPLOYED'] = raw_app['DAYS_EMPLOYED'] // (-365)
+    raw_app['AGE'] = raw_app['DAYS_BIRTH'] // (-365)
+    raw_app['CREDIT'] = raw_app['AMT_CREDIT']   
 #raw_app['CREDIT'] = raw_app['AMT_CREDIT'].apply(lambda x: 'No' if math.isnan(x) else 'Yes')       
 
 st.dataframe(raw_app)
