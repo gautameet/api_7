7f66b9da16df556f7ca21806d80e0ed4b0d99883
gautameet <145467217+gautameet@users.noreply.github.com>
2024-01-27 07:45:04 +0100

    Update dash_test.py

--- dash_test.py
+++ dash_test.py
@@ -280,84 +280,84 @@ try:
 ## DASH BOARD
 ########################
 
-## Page configuration initialisation
-st.set_page_config(
-    page_title="Credit Score Dashboard",
-    page_icon="💵",
-    layout="wide",
-    initial_sidebar_state="expanded")
-
-# Sidebar
-with st.sidebar:
-    st.write("Credit Score Dashboard")
-    logo_path = "logo.png"
-    try:
-        logo = Image.open(logo_path)
-        st.image(logo, width=250)
-    except FileNotFoundError:
-        st.error(f"Error: Logo file not found at {logo_path}")
-
-# Page selection
-    page =  st.selectbox("Menu", ["Home", "Customer"])
-
-
+    ## Page configuration initialisation
+    st.set_page_config(
+        page_title="Credit Score Dashboard",
+        page_icon="💵",
+        layout="wide",
+        initial_sidebar_state="expanded")
     
-    st.markdown("""---""")
+    # Sidebar
+    with st.sidebar:
+        st.write("Credit Score Dashboard")
+        logo_path = "logo.png"
+        try:
+            logo = Image.open(logo_path)
+            st.image(logo, width=250)
+        except FileNotFoundError:
+            st.error(f"Error: Logo file not found at {logo_path}")
     
-    st.write("By: Amit GAUTAM")
-
-
-
-if page == "Home":
-    st.title("💵 Credit Score Dashboard - Home Page")
-    ".\n"
-    #.\n"
+    # Page selection
+        page =  st.selectbox("Menu", ["Home", "Customer"])
     
-    st.markdown("This is an interactive dashboard website which lets the clients to know about their credit demands\n"
-                "approved ou refused. The predictions are calculted automatically with the help of machine learning algorithm.\n"
-                                
-                "\nThis dashboard is composed of following pages :\n"
-                "- **Customer**: to find out all the information related to the customer.\n")
-                
-if page == "Customer":
-    st.title("💵 Welcome to the Customer Page")
-    ".\n"
-    #.\n"
     
-    st.header("Welcome to the customers' page.\n")
-    ".\n"
-    st.subheader("Please enter your ID to know the results of your demands. \n") 
-    #"Thank you. \n"
-    st.markdown("Your ID:")
-    Cst_ID=st.number_input(" ", min_value=100002, max_value=456255)
+        
+        st.markdown("""---""")
+        
+        st.write("By: Amit GAUTAM")
+    
 
+
+    if page == "Home":
+        st.title("💵 Credit Score Dashboard - Home Page")
+        ".\n"
+        #.\n"
+        
+        st.markdown("This is an interactive dashboard website which lets the clients to know about their credit demands\n"
+                    "approved ou refused. The predictions are calculted automatically with the help of machine learning algorithm.\n"
+                                    
+                    "\nThis dashboard is composed of following pages :\n"
+                    "- **Customer**: to find out all the information related to the customer.\n")
+                    
+    if page == "Customer":
+        st.title("💵 Welcome to the Customer Page")
+        ".\n"
+        #.\n"
+        
+        st.header("Welcome to the customers' page.\n")
+        ".\n"
+        st.subheader("Please enter your ID to know the results of your demands. \n") 
+        #"Thank you. \n"
+        st.markdown("Your ID:")
+        Cst_ID=st.number_input(" ", min_value=100002, max_value=456255)
+    
+         
+        #with st.sidebar:
+            #st.selectbox("Enter your ID", "      ")
+            #button_start = st.button("Submit")
+        
+        
+        
+                    #if button_start:
+    # Use the entered customer_id to fetch and display relevant information
+    # Add your logic here
+                    #customer_info = fetch_customer_info("Navigation")
+                    #st.write(customer_info)
+    
+        #Id selection
      
-    #with st.sidebar:
-        #st.selectbox("Enter your ID", "      ")
-        #button_start = st.button("Submit")
+    # First, create the layout with three columns
+        col1, col2, col3 = st.columns(3)
     
+    # Now, add content to each column
+        with col1:
+            st.header("Column 1")
+            st.write("Content for column 1 goes here.")
     
+        with col2:
+            st.header("Column 2")
+            st.write("Content for column 2 goes here.")
     
-                #if button_start:
-# Use the entered customer_id to fetch and display relevant information
-# Add your logic here
-                #customer_info = fetch_customer_info("Navigation")
-                #st.write(customer_info)
-
-    #Id selection
- 
-# First, create the layout with three columns
-    col1, col2, col3 = st.columns(3)
-
-# Now, add content to each column
-    with col1:
-        st.header("Column 1")
-        st.write("Content for column 1 goes here.")
-
-    with col2:
-        st.header("Column 2")
-        st.write("Content for column 2 goes here.")
-
-    with col3:
-        st.header("Column 3")
-        st.write("Content for column 3 goes here.")
+        with col3:
+            st.header("Column 3")
+            st.write("Content for column 3 goes here.")
