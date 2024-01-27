ac5344aeba6a24bb8f22d58c54a24a3c9ad9ddb7
gautameet <145467217+gautameet@users.noreply.github.com>
2024-01-27 06:25:30 +0100

    Update dash_test.py

--- dash_test.py
+++ dash_test.py
@@ -53,10 +53,9 @@ except Exception as e:
 #del raw_test
 
 #print(raw_app.columns)
-
-raw_app['AGE'] = raw_app['DAYS_BIRTH'] // (-365)
-raw_app['YEARS_EMPLOYED'] = raw_app['DAYS_EMPLOYED'] // (-365)
-raw_app['CREDIT'] = raw_app['AMT_CREDIT'].apply(lambda x: 'No' if math.isnan(x) else 'Yes')
+        raw_app['AGE'] = raw_app['DAYS_BIRTH'] // (-365)
+        raw_app['YEARS_EMPLOYED'] = raw_app['DAYS_EMPLOYED'] // (-365)
+        raw_app['CREDIT'] = raw_app['AMT_CREDIT'].apply(lambda x: 'No' if math.isnan(x) else 'Yes')
 
 # Drop unnecessary columns
     #raw_app = raw_app.drop(['DAYS_BIRTH','DAYS_EMPLOYED'], axis=1)
