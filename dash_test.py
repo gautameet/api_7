ed25168847a98407417515740a57be7a2714d08a
gautameet <145467217+gautameet@users.noreply.github.com>
2024-01-27 04:50:43 +0100

    Update dash_test.py

--- dash_test.py
+++ dash_test.py
@@ -21,9 +21,9 @@ feat = ['SK_ID_CURR','TARGET','DAYS_BIRTH','NAME_FAMILY_STATUS','CNT_CHILDREN',
 #num_rows = 150000
 
 # Original Data
-zip_file_train = 'sample_application_train.zip'
-with ZipFile(zip_file_train, 'r') as zip_train:
-        raw_train = pd.read_csv(zip_train.open('sample_application_train.csv'), usecols=feat)
+zip_file_train = ZipFile('sample_application_train.zip')
+#with ZipFile(zip_file_train, 'r') as zip_train:
+raw_train = pd.read_csv(zip_file_train.open('sample_application_train.csv'), usecols=feat)
 
 
 zip_file_test= 'application_test.zip'
