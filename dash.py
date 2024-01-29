2f0789fd3304669679e5c418141238072b9fa89b
gautameet <145467217+gautameet@users.noreply.github.com>
2024-01-29 17:38:03 +0100

    Update dash.py

--- dash.py
+++ dash.py
@@ -50,7 +50,11 @@ except Exception as e:
     # Print the exception message for debugging
     print(f"Error concatenating DataFrames: {e}")
 
-#raw_app['DAYS_BIRTH'] = pd.to_numeric(raw_app['DAYS_BIRTH'], errors='coerce')
+    # Convert 'DAYS_BIRTH' to numeric and handle non-numeric values
+raw_app['DAYS_BIRTH'] = pd.to_numeric(raw_app['DAYS_BIRTH'], errors='coerce')
+    # Check if 'DAYS_BIRTH' is not zero before performing the division
+raw_app['AGE'] = raw_app['DAYS_BIRTH'] // (-365) if 0 not in raw_app['DAYS_BIRTH'].values else 0
+    #raw_app['DAYS_BIRTH'] = pd.to_numeric(raw_app['DAYS_BIRTH'], errors='coerce')
 raw_app['AGE'] = raw_app['DAYS_BIRTH'] // (-365)
 raw_app['YEARS_EMPLOYED'] = raw_app['DAYS_EMPLOYED'] // (-365)
 raw_app['CREDIT'] = raw_app['AMT_CREDIT'].apply(lambda x: 'No' if math.isnan(x) else 'Yes')
