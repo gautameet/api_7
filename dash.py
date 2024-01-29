4adc43c1d32c79082904e69f24471a3ef04cb944
gautameet <145467217+gautameet@users.noreply.github.com>
2024-01-29 12:12:40 +0100

    Update dash.py

--- dash.py
+++ dash.py
@@ -21,7 +21,7 @@ feat = ['SK_ID_CURR','TARGET','DAYS_BIRTH','NAME_FAMILY_STATUS','CNT_CHILDREN','
 num_rows = 100000
 
 # Original Data
-zip_file_train = ZipFile('./sample_application_train.zip')
+zip_file_train = ZipFile('sample_application_train.zip')
 print(zip_file_train.namelist())
 
 try:
