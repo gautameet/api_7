2f93d4436af2b4d542a52fd958adb4c9ac76507f
gautameet <145467217+gautameet@users.noreply.github.com>
2024-01-30 21:13:01 +0100

    Update dash_test.py

--- dash_test.py
+++ dash_test.py
@@ -15,6 +15,7 @@ import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
 
+
 # Features
 feat = ['SK_ID_CURR','TARGET','DAYS_BIRTH','NAME_FAMILY_STATUS','CNT_CHILDREN',
         'DAYS_EMPLOYED','NAME_INCOME_TYPE','AMT_INCOME_TOTAL','AMT_CREDIT','AMT_ANNUITY']
