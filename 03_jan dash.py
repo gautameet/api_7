318f459723d8f3d6628cd0c286f3efbed923eb8a
gautameet <145467217+gautameet@users.noreply.github.com>
2024-01-30 16:38:46 +0100

    Update 03_jan dash.py

--- 03_jan dash.py
+++ 03_jan dash.py
@@ -15,32 +15,3 @@ from zipfile import ZipFile, BadZipFile
 import pickle
 
 
-# Features
-feat = ['SK_ID_CURR','TARGET','DAYS_BIRTH','NAME_FAMILY_STATUS','CNT_CHILDREN','DAYS_EMPLOYED','NAME_INCOME_TYPE','AMT_INCOME_TOTAL','AMT_CREDIT','AMT_ANNUITY']
-
-# Nombre de ligne
-num_rows = 100000
-
-# Original Data
-zip_file_path = 'sample_application_train.zip'
-try:
-    with ZipFile(zip_file_path, 'r') as zip_file:
-       raw_train = pd.read_csv(zip_file.open('sample_application_train.csv'), usecols=feat, nrows=num_rows) 
-except BadZipFile:
-    print(f"Error: '{zip_file_path}' is not a valid ZIP file.")
-except Exception as e:
-    print(f'An unexpected error occured: {e}')
-
-try:
-    raw_test = pd.read_csv(zip_file_test.open('application_test.csv'),usecols=[f for f in feat if f!='TARGET'], nrows=num_rows)
-except Exception as e:
-    print(f'Error reading test data:{e}')        
-
-try:
-    raw_app = raw_train.append(raw_test).reset_index(drop=True)    # Append the DataFrames
-    #raw_app = pd.concat([raw_train, raw_test], ignore_index=True)
-
-except Exception as e:
-    # Print the exception message for debugging
-    print(f"Error concatenating DataFrames: {e}")
-
