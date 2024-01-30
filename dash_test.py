7480f12c4a4cc912bc80f5bc730ef08c994f08b2
gautameet <145467217+gautameet@users.noreply.github.com>
2024-01-30 20:12:29 +0100

    Update dash_test.py

--- dash_test.py
+++ dash_test.py
@@ -421,7 +421,8 @@ except Exception as e:
                     plt.setp(pt.get_yticklabels(),fontsize=7)
                     st.pyplot(fig)
                 st.markdown("-----")
-            
+
+                
             with st.container():
                 st.write("#### Loan Payment")
                 tg_n = np.array([len(raw_app[raw_app['TARGET']==1]),len(raw_app[raw_app['TARGET']==0]),len(raw_app[raw_app['TARGET'].isnull()])])            
