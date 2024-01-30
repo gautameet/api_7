60302b3d137fee0fc866f01524306b7b11692b5b
gautameet <145467217+gautameet@users.noreply.github.com>
2024-01-30 16:48:59 +0100

    Update dash.py

--- dash.py
+++ dash.py
@@ -18,7 +18,7 @@ import pickle
 feat = ['SK_ID_CURR','TARGET','DAYS_BIRTH','NAME_FAMILY_STATUS','CNT_CHILDREN','DAYS_EMPLOYED','NAME_INCOME_TYPE','AMT_INCOME_TOTAL','AMT_CREDIT','AMT_ANNUITY']
 
 # Nombre de ligne
-num_rows = 100000
+num_rows = 150000
 
 # Original Data
 zip_file_path = 'sample_application_train.zip'
