a7f30e922e5c487da6344754c2979f00b7028b7c
gautameet <145467217+gautameet@users.noreply.github.com>
2024-01-30 22:04:26 +0100

    Update dash_test.py

--- dash_test.py
+++ dash_test.py
@@ -11,9 +11,9 @@ import plotly
 import os
 from zipfile import ZipFile
 import pickle
-import matplotlib
+#import matplotlib
 #matplotlib.use('agg')
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 
 # Features
