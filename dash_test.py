7149f0dacf59fef35b9967305a322222503f989b
gautameet <145467217+gautameet@users.noreply.github.com>
2024-02-03 00:15:27 +0100

    Update dash_test.py

--- dash_test.py
+++ dash_test.py
@@ -1,19 +1,22 @@
 ## Import des librairies
-import math
-import shap
 import streamlit as st
 import altair as alt              # for data visualtization
-#from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import NearestNeighbors
+#from sklearn.preprocessing import StandardScaler
 from PIL import Image
 import requests
 import plotly
 import os
 from zipfile import ZipFile
 import pickle
+import matplotlib.pyplot as plt
+import math
+import shap
+
+
 #import matplotlib
 #matplotlib.use('agg')
-import matplotlib.pyplot as plt
+
 
 
 # Features
