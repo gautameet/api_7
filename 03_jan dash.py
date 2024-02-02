efaff58f3c05df2a188d16d49ab25820c8a02994
gautameet <145467217+gautameet@users.noreply.github.com>
2024-02-02 18:34:43 +0100

    Update 03_jan dash.py

--- 03_jan dash.py
+++ 03_jan dash.py
@@ -1,12 +1,7 @@
  ## Import des librairies
-import math
 import streamlit as st
 import altair as alt              # for data visualtization
 from PIL import Image
 import requests
-import plotly
-import os
-from zipfile import ZipFile, BadZipFile
-import pickle
 
 
