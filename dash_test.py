a34ae100d67df2cd0c17a802f8f6bbf1f30f0f5a
gautameet <145467217+gautameet@users.noreply.github.com>
2024-02-03 05:08:30 +0100

    Update dash_test.py

--- dash_test.py
+++ dash_test.py
@@ -135,18 +135,17 @@ knn = NearestNeighbors(n_neighbors=10)
 knn.fit(train.drop(['SK_ID_CURR','TARGET'], axis=1))
 
     # Chargement du modèle de classification
-pk_mdl_in = open('./Results/model.pkl','rb')
+pk_mdl_in = open('model.pkl','rb')
 model = pickle.load(pk_mdl_in)
 
-
     # Explainer
-zip_file = ZipFile('./Results/X_train_sm_split1.zip')
+zip_file = ZipFile('X_train_sm_split1.zip')
 X_train_sm_1 = pd.read_csv(zip_file.open('X_train_sm_split1.csv'))
 
-zip_file = ZipFile('./Results/X_train_sm_split2.zip')
+zip_file = ZipFile('X_train_sm_split2.zip')
 X_train_sm_2 = pd.read_csv(zip_file.open('X_train_sm_split2.csv'))
 
-zip_file = ZipFile('./Results/X_train_sm_split3.zip')
+zip_file = ZipFile('X_train_sm_split3.zip')
 X_train_sm_3 = pd.read_csv(zip_file.open('X_train_sm_split3.csv'))
 
 X_train_sm = X_train_sm_split1.append(X_train_sm_split2).reset_index(drop=True).append(X_train_sm_split3).reset_index(drop=True)
