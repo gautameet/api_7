01e0b0b4898b40eabd12f169b52105b1ae2f2580
gautameet <145467217+gautameet@users.noreply.github.com>
2024-02-03 05:47:39 +0100

    Update dash_test.py

--- dash_test.py
+++ dash_test.py
@@ -32,11 +32,10 @@ num_rows = 100000
 
 # Original Data
 zip_file_train = ZipFile('sample_application_train.zip')
-#zip_file_train = ZipFile('./sample_application_train.zip')
-#print(zip_file_train.namelist())
-
 raw_train = pd.read_csv(zip_file_train.open('sample_application_train.csv'), usecols=feat, nrows=num_rows)
 
+#zip_file_train = ZipFile('./sample_application_train.zip')
+#print(zip_file_train.namelist())
 #try:
     #raw_train = pd.read_csv(zip_file_train.open('sample_application_train.csv'), usecols=feat, nrows=num_rows)
 #with ZipFile(zip_file_train, 'r') as zip_train:
