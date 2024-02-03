3a641b7314745d0869d7ee3128bb865e754dff99
gautameet <145467217+gautameet@users.noreply.github.com>
2024-02-03 04:52:03 +0100

    Update dash_test.py

--- dash_test.py
+++ dash_test.py
@@ -76,9 +76,10 @@ raw_app = raw_app.drop(['DAYS_BIRTH','DAYS_EMPLOYED'], axis=1)
 # Treated Data
 
 zip_file_path = './data_train.zip'
-csv_file_name = 'data_train.csv'
+train = pd.read_csv(zip_file_path.open(data_train.csv))
 
-train = pd.read_csv(zip_file_path.open(csv_file_name))
+#csv_file_name = 'data_train.csv'
+#train = pd.read_csv(zip_file_path.open(csv_file_name))
 
 #try:
     # Open the ZIP file
