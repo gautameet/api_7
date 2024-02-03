4da1cf9ff4b23e3f050f0af20f7628e54b8ea23c
gautameet <145467217+gautameet@users.noreply.github.com>
2024-02-03 05:20:17 +0100

    Update dash_test.py

--- dash_test.py
+++ dash_test.py
@@ -136,9 +136,9 @@ knn = NearestNeighbors(n_neighbors=10)
 knn.fit(train.drop(['SK_ID_CURR','TARGET'], axis=1))
 
 # Chargement du modèle de classification
-model = pickle.load(open('model.pkl','rb'))
-#pk_mdl_in = open('model.pkl','r')
-#model = pickle.load(pk_mdl_in)
+{model = pickle.load(open('model.pkl','rb'))
+pk_mdl_in = open('model.pkl','rb')
+model = pickle.load(pk_mdl_in)
 
     # Explainer
 zip_file = ZipFile('X_train_sm_split1.zip')
