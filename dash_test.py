aa15b4bf24a3afdaaf5786785390c939b35e0462
gautameet <145467217+gautameet@users.noreply.github.com>
2024-02-03 10:22:59 +0100

    Update dash_test.py

--- dash_test.py
+++ dash_test.py
@@ -349,7 +349,7 @@ if page == 'Customer portfolio':
             plt.ioff()
             with col1:
                 fig = plt.figure(figsize=(4,4))
-                bins = int(raw_app['AGE'].max()-raw_app['AGE'].min())//5)
+                bins = int(raw_app['AGE'].max()-raw_app['AGE'].min()//5)
                 pt = sns.histplot(data=raw_app, x='AGE', hue='TARGET',bins=bins,palette=['royalblue','red'],alpha=.5)
                 plt.xlabel('AGE',fontsize=12)
                 plt.ylabel('')
