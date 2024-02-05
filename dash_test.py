dbe1392f08f35cceba9137cfb14020bdeaa54e4c
gautameet <145467217+gautameet@users.noreply.github.com>
2024-02-05 15:11:18 +0100

    Update dash_test.py

--- dash_test.py
+++ dash_test.py
@@ -161,18 +161,18 @@ def _scale_data(data, ranges):
     for d, (y1, y2) in zip(data[1:], ranges[1:]):
         assert (y1 <= d <= y2) or (y2 <= d <= y1)
     x1, x2 = ranges[0]
-        d = data[0]
-        if x1 > x2:
-            d = _invert(d, (x1, x2))
-            x1, x2 = x2, x1
-        sdata = [d]
-        for d, (y1, y2) in zip(data[1:], ranges[1:]):
-            if y1 > y2:
-                d = _invert(d, (y1, y2))
-                y1, y2 = y2, y1
-            sdata.append((d-y1) / (y2-y1) 
+    d = data[0]
+    if x1 > x2:
+        d = _invert(d, (x1, x2))
+        x1, x2 = x2, x1
+    sdata = [d]
+    for d, (y1, y2) in zip(data[1:], ranges[1:]):
+        if y1 > y2:
+            d = _invert(d, (y1, y2))
+            y1, y2 = y2, y1
+        sdata.append((d-y1) / (y2-y1) 
                          * (x2 - x1) + x1)
-        return sdata
+    return sdata
 
 class ComplexRadar():
     def __init__(self, fig, variables, ranges, n_ordinate_levels=6):
