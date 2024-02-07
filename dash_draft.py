b50ac7475660d9cfbda8b772aec59574ba1ce4ab
gautameet <145467217+gautameet@users.noreply.github.com>
2024-02-07 07:41:33 +0100

    Update dash_draft.py

--- dash_draft.py
+++ dash_draft.py
@@ -215,7 +215,7 @@ def radat_id_plot(ID,fig,features=features,fill=False,raw_app=None):
 
 def radat_knn_plot(ID, fig, features, fill=False, raw_app=raw_app, get_data=None, get_similar_ID=None):
     # Get data for the specified customer ID
-    app_id = get_data(raw_app,ID).loc[:,features]
+    app_id = get_data(raw_app,ID)[features]
     #app_id = get_data(raw_app,ID).loc[:,features]
     data_id = app_id.iloc[0]    
 
