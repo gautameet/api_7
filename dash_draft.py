1a74fa52457d9bdbb4f214e9935fb8d2eb777a3b
gautameet <145467217+gautameet@users.noreply.github.com>
2024-02-07 06:33:08 +0100

    Update dash_draft.py

--- dash_draft.py
+++ dash_draft.py
@@ -341,7 +341,7 @@ if page == "Customer":
             col3, col4 = st.columns([3,1])
             with col3:
                 fig = plt.figure(figsize=(3,3))
-                radat_knn_plot(ID,fig,features=features,raw_app=raw_app)
+                radat_knn_plot(ID,fig,features=features)
                 #radat_knn_plot(ID,fig,features=features,raw_app=raw_app,get_data=get_data, get_similar_ID=get_similar_ID)
                 st.pyplot(fig)
             with col4:
