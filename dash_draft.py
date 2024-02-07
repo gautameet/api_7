1ed8ccdb26f1b8a770972fce60e4c4c6361448f1
gautameet <145467217+gautameet@users.noreply.github.com>
2024-02-07 05:10:56 +0100

    Update dash_draft.py

--- dash_draft.py
+++ dash_draft.py
@@ -1,16 +1,50 @@
-import math
-import shap
+## Import des librairies
 import streamlit as st
 import altair as alt              # for data visualtization
-#from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import NearestNeighbors
-from PIL import Image
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
 import requests
 import plotly
+import matplotlib.pyplot as plt
+import shap
+import seaborn as sns
+import pandas as pd
+import numpy as np
 import os
-from zipfile import ZipFile
+from zipfile import ZipFile, BadZipFile
+import joblib
 import pickle
-import matplotlib
+import math
+import json
+#import Pillow
+from PIL import Image
 
-#matplotlib.use('agg')
-#import matplotlib.pyplot as plt
+# Features
+feat = ['SK_ID_CURR','TARGET','DAYS_BIRTH','NAME_FAMILY_STATUS','CNT_CHILDREN',
+        'DAYS_EMPLOYED','NAME_INCOME_TYPE','AMT_INCOME_TOTAL','AMT_CREDIT','AMT_ANNUITY']
+
+# Nombre de ligne
+num_rows = 100000
+
+# Original Data
+zip_file_train = ZipFile('sample_application_train.zip')
+raw_train = pd.read_csv(zip_file_train.open('sample_application_train.csv'),usecols=feat, nrows=num_rows)
+
+zip_file_test = ZipFile('./application_test.zip')
+raw_test = pd.read_csv(zip_file_test.open('application_test.csv'),usecols=[f for f in feat if f!='TARGET'])
+
+raw_app = pd.concat([raw_train, raw_test], ignore_index=True)        #concat
+del raw_train
+del raw_test
+ 
+raw_app.loc[:, 'YEARS_EMPLOYED'] = raw_app['DAYS_EMPLOYED'].apply(lambda x: -x/-365)
+raw_app.loc[:, 'AGE'] = raw_app['DAYS_BIRTH'].apply(lambda x: -x/-365) // (-365)
+#raw_app['CREDIT'] = raw_app['AMT_CREDIT']   
+raw_app.loc[:, 'CREDIT'] = raw_app['AMT_CREDIT'].apply(lambda x: 'No' if math.isnan(x) else 'Yes')       
+
+#st.dataframe(raw_app)
+
+# Drop unnecessary columns
+raw_app = raw_app.drop(['DAYS_BIRTH','DAYS_EMPLOYED'], axis=1).copy()
