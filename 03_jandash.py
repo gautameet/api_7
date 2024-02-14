ab446732c3aee20ceb3fc98f9db5cd9c5d20b4d7
gautameet <145467217+gautameet@users.noreply.github.com>
2024-02-14 18:45:19 +0100

    Update 03_jandash.py

--- 03_jandash.py
+++ 03_jandash.py
@@ -334,7 +334,8 @@ if page == "Information du client":
     # Affichage des informations client
     with st.expander("Afficher les informations du client", expanded=False):
         st.info("Voici les informations du client:")
-        st.write(pd.DataFrame(data_test.loc[data_test['SK_ID_CURR'] == id_client_dash]))
+        #st.write(pd.DataFrame(data_test.loc[data_test['SK_ID_CURR'] == id_client_dash]))
+        st.write(pd.DataFrame(data_test.loc[data_test['SK_ID_CURR'] == client_id]))
 
 
 if page == "Interprétation locale":
@@ -398,16 +399,20 @@ if page == "Interprétation globale":
                 feature1 = st.selectbox("Choisissez une caractéristique", list_features,
                                         index=list_features.index('AMT_CREDIT'))
                 if distrib_compa == 'Tous':
-                    distribution(feature1, id_client_dash, data_train)
+                    #distribution(feature1, id_client_dash, data_train)
+                    distribution(feature1, client_id, data_train)
                 else:
-                    distribution(feature1, id_client_dash, data_voisins)
+                    distribution(feature1, client_id, data_voisins)
+                    #distribution(feature1, id_client_dash, data_voisins)
             with col2:
                 feature2 = st.selectbox("Choisissez une caractéristique", list_features,
                                         index=list_features.index('EXT_SOURCE_2'))
                 if distrib_compa == 'Tous':
-                    distribution(feature2, id_client_dash, data_train)
+                    distribution(feature2, client_id, data_train)
+                    #distribution(feature2, id_client_dash, data_train)
                 else:
-                    distribution(feature2, id_client_dash, data_voisins)
+                    distribution(feature2, client_id, data_voisins)
+                    #distribution(feature2, id_client_dash, data_voisins)
 
             with st.expander("Explication des distributions", expanded=False):
                 st.caption("Vous pouvez sélectionner la caractéristique dont vous souhaitez observer la distribution. "
@@ -435,9 +440,11 @@ if page == "Interprétation globale":
         # Affichage des nuages de points de la feature 2 en fonction de la feature 1
         if (feat1 != '<Select>') & (feat2 != '<Select>'):
             if bivar_compa == 'Tous':
-                scatter(id_client_dash, feat1, feat2, data_train)
+                scatter(client_id, feat1, feat2, data_train)
+                #scatter(id_client_dash, feat1, feat2, data_train)
             else:
-                scatter(id_client_dash, feat1, feat2, data_voisins)
+                scatter(client_id, feat1, feat2, data_voisins)
+                #scatter(id_client_dash, feat1, feat2, data_voisins)
             with st.expander("Explication des scatter plot", expanded=False):
                 st.caption("Vous pouvez ici afficher une caractéristique en fonction d'une autre. "
                            "En bleu sont indiqués les clients ne faisant pas défaut et dont le prêt est jugé comme "
@@ -456,7 +463,8 @@ if page == "Interprétation globale":
                                   default=['AMT_CREDIT', 'AMT_ANNUITY', 'EXT_SOURCE_2', 'EXT_SOURCE_3'])
 
         # Affichage des boxplot
-        boxplot_graph(id_client_dash, features, data_voisins)
+        boxplot_graph(client_id, features, data_voisins)
+        #boxplot_graph(id_client_dash, features, data_voisins)
         with st.expander("Explication des boxplot", expanded=False):
             st.caption("Les boxplot permettent d'observer les distributions des variables renseignées. "
                        "Une étoile violette représente le client. Ses plus proches voisins sont également "
