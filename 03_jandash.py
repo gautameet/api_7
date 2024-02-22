db0e4c0d6da1f51811d603cf8b6a834a0c437574
gautameet <145467217+gautameet@users.noreply.github.com>
2024-02-22 14:04:33 +0100

    Update 03_jandash.py

--- 03_jandash.py
+++ 03_jandash.py
@@ -390,7 +390,7 @@ if page == "Information du client":
             st.markdown("RÉSULTAT DE LA DEMANDE")
 
             # Call the function and assign the return value to a single variable
-            probability, decision = prediction(id_client_dash)
+            probability, decision = prediction(id_client)
 
             if probability is not None and decision is not None:
                 st.write(f"Probability of Default: {probability}")
