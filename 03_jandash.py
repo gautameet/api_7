0fa9115728e9fa11e223c0e782b942dc10b85058
gautameet <145467217+gautameet@users.noreply.github.com>
2024-02-27 04:06:23 +0100

    Update 03_jandash.py

--- 03_jandash.py
+++ 03_jandash.py
@@ -327,15 +327,15 @@ if page == "Information du client":
 			
 			# Call the function and assign the return value to a single variable
 		
-		proba_default, decision = prediction(client_id)
-		if decision is None:  # Check if only one value is returned (error message)
-			st.write(proba_default)  # Display the error message
-		else:  # Two values returned (probability and decision)
-			st.write(f"Probability of Default: {proba_default}")
-			st.write(f"Decision: {decision}")
+			proba_default, decision = prediction(id_client_dash)
+			if decision is None:  # Check if only one value is returned (error message)
+				st.write(proba_default)  # Display the error message
+			else:  # Two values returned (probability and decision)
+				st.write(f"Probability of Default: {proba_default}")
+				st.write(f"Decision: {decision}")
 			
-		# Affichage de la jauge
-		jauge_score(proba_default)
+			# Affichage de la jauge
+			jauge_score(proba_default)
             
     # Affichage des informations client
 with st.expander("Afficher les informations du client", expanded=False):
