ba0d5cebd56baaf51fde54dd09301113ed0d431c
gautameet <145467217+gautameet@users.noreply.github.com>
2024-02-27 06:55:20 +0100

    Update 03_jandash.py

--- 03_jandash.py
+++ 03_jandash.py
@@ -157,8 +157,8 @@ def df_voisins(client_id: int, data_train, data_test):
     """
     try:
 	features = list(data_train.columns)
-    features.remove('SK_ID_CURR')
-    features.remove('TARGET')
+    	features.remove('SK_ID_CURR')
+    	features.remove('TARGET')
 
     # Creating an instance of NearestNeighbors
     knn = NearestNeighbors(n_neighbors=10, metric='euclidean')
