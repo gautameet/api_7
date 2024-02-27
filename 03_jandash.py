e5f692632a53254c569026d18b436cf8ec63d489
gautameet <145467217+gautameet@users.noreply.github.com>
2024-02-27 10:41:12 +0100

    Update 03_jandash.py

--- 03_jandash.py
+++ 03_jandash.py
@@ -500,12 +500,12 @@ if bivar:
 				scatter(id_client_dash, feat1, feat2, data_train)
 			else:
 				scatter(id_client_dash, feat1, feat2, data_voisins)
-        		with st.expander("Explication des scatter plot", expanded=False):
-        		st.caption("Vous pouvez ici afficher une caractéristique en fonction d'une autre. "
-                           "En bleu sont indiqués les clients ne faisant pas défaut et dont le prêt est jugé comme "
-                           "accordé. En rouge, sont indiqués les clients faisant défaut et dont le prêt est jugé "
-                           "comme refusé. L'étoile noire correspond au client et permet donc de le situer par rapport "
-                           "à la base de données clients.")
+			with st.expander("Explication des scatter plot", expanded=False):
+				st.caption("Vous pouvez ici afficher une caractéristique en fonction d'une autre. "
+                           	"En bleu sont indiqués les clients ne faisant pas défaut et dont le prêt est jugé comme "
+                           	"accordé. En rouge, sont indiqués les clients faisant défaut et dont le prêt est jugé "
+                           	"comme refusé. L'étoile noire correspond au client et permet donc de le situer par rapport "
+                           	"à la base de données clients.")
 
 boxplot = st.checkbox("Analyse des boxplot")
 if boxplot:
