6f6f9cbe54c8a850b754b110f01348f733e4995c
gautameet <145467217+gautameet@users.noreply.github.com>
2024-02-27 11:15:14 +0100

    Update 03_jandash.py

--- 03_jandash.py
+++ 03_jandash.py
@@ -442,10 +442,10 @@ if page == "Interprétation globale":
 		data_test_std = minmax_scale(data_test.drop('SK_ID_CURR', axis=1), 'std')
 		nb_features = st.slider('Nombre de variables à visualiser', 0, 20, 10)
 		fig, ax = plt.subplos()
-    
-    # Affichage du summary plot : shap global
-    ax = shap.summary_plot(shap_values, data_test_std, plot_type='bar', max_display=nb_features)
-    st.pyplot(fig)
+		
+		# Affichage du summary plot : shap global
+		ax = shap.summary_plot(shap_values, data_test_std, plot_type='bar', max_display=nb_features)
+    		st.pyplot(fig)
 
 with st.expander("Explication du graphique", expanded=False):
     st.caption("Ici sont affichées les caractéristiques influençant de manière globale la décision.")
