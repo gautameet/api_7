accc39182d3f6f575eda0bc9ef89f564b00700a1
gautameet <145467217+gautameet@users.noreply.github.com>
2024-02-28 12:25:42 +0100

    Update dash_draft.py

--- dash_draft.py
+++ dash_draft.py
@@ -14,7 +14,7 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 import os
-from zipfile import ZipFile
+from zipfile import ZipFile, BadZipFile
 import joblib
 import pickle
 import math
